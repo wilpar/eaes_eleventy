06292b6c255ae18a530419f2cf05c34c447ca4cc
Rob H <rob@extraservings.com>
2021-02-01 13:18:10 -0500

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addWatchTarget("./_tmp/style.css");
+  eleventyConfig.addPassthroughCopy({ 
+    "./_tmp/style.css": "./style.css",
+    "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
+  });
+  eleventyConfig.addShortcode("version", function () {
+    return String(Date.now());
+  });
+  
+  return {
+    dir: {
+        input: "src/",
+    },
+    templateFormats: ["html", "md", "njk"],
+    htmlTemplateEngine: "njk",
+    passthroughFileCopy: true
+  };
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# eaes_eleventy
