b2435ce44ba6b75a4cb22abfd1d46eca447f79a7
Rob H <rob@extraservings.com>
2021-02-03 13:19:32 -0500

    add Assets directory passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 
     "./_tmp/style.css": "./style.css",
     "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
+    "src/_assets": "./assets"
   });
   eleventyConfig.addShortcode("version", function () {
     return String(Date.now());
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # eaes_eleventy
+
+This is a template for Eleventy on Netlify, using TailwindCSS and Alpine.
+
+Don't forget to run npm install and npm run build before you attempt to use this.
+
+
+
